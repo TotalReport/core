1a9fbe5d6b3499b486bfd821ea46bdd2e252b683
Igor Lunin <lunigorn@gmail.com>
2024-09-23 20:09:31 +0200

    Add arguments hash tests to before tests creation

--- test/tests/before-tests.test.ts
+++ test/tests/before-tests.test.ts
@@ -2,11 +2,12 @@ import { expect } from "earl";
 import { describe, test } from "mocha";
 import { client } from "../tools/client.js";
 import { generateLaunch } from "../tools/launch-generator.js";
+import { BeforeTestsGenerator } from "../tools/before-test-generator.js";
 
 describe("before tests", () => {
   test("create before test", async () => {
     const launch = await generateLaunch();
-    const response =  await client.createBeforeTest({
+    const response = await client.createBeforeTest({
       body: {
         title: "New before test",
         launchId: launch.id,
@@ -47,8 +48,79 @@ describe("before tests", () => {
             type: "Integer",
             value: "value2",
           },
-        ],        
-      }
-    })
+        ],
+      },
+    });
+  });
+
+  test("arguments hash is same for same arguments", async () => {
+    const beforeTestsGenerator = new BeforeTestsGenerator(client);
+    const beforeTestsArguments = [
+      {
+        name: "Argument1",
+        type: "String",
+        value: "value1",
+      },
+      {
+        name: "Argument2",
+        type: "Integer",
+        value: "value2",
+      },
+    ];
+
+    const first = await beforeTestsGenerator.create({
+      arguments: beforeTestsArguments,
+    });
+    const second = await beforeTestsGenerator.create({
+      arguments: beforeTestsArguments,
+    });
+
+    expect(first.arguments?.map((arg) => arg.id)).not.toEqual(
+      second.arguments?.map((arg) => arg.id)
+    );
+
+    expect(first.argumentsHash).toEqual(second.argumentsHash);
+  });
+
+  test("arguments hash is different for different arguments", async () => {
+    const beforeTestsGenerator = new BeforeTestsGenerator(client);
+    const beforeTestsArguments1 = [
+      {
+        name: "Argument1",
+        type: "String",
+        value: "value1",
+      },
+      {
+        name: "Argument2",
+        type: "Integer",
+        value: "value2",
+      },
+    ];
+
+    const beforeTestsArguments2 = [
+      {
+        name: "Argument1",
+        type: "String",
+        value: "value1",
+      },
+      {
+        name: "Argument3",
+        type: "Integer",
+        value: "value2",
+      },
+    ];
+
+    const first = await beforeTestsGenerator.create({
+      arguments: beforeTestsArguments1,
+    });
+    const second = await beforeTestsGenerator.create({
+      arguments: beforeTestsArguments2,
+    });
+
+    expect(first.arguments?.map((arg) => arg.id)).not.toEqual(
+      second.arguments?.map((arg) => arg.id)
+    );
+
+    expect(first.argumentsHash).not.toEqual(second.argumentsHash);
   });
 });
