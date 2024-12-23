685a4709dfeece30e7a2977bb9fb231d2efc718b
Igor Lunin <lunigorn@gmail.com>
2024-12-23 08:26:13 +0100

    Add earl matcher equalUnsorted

--- test/tools/earl-extensions.ts
+++ test/tools/earl-extensions.ts
@@ -1,4 +1,4 @@
-import { registerMatcher } from "earl";
+import { isEqual, registerMatcher } from "earl";
 
 declare module "earl" {
   interface Matchers {
@@ -7,10 +7,18 @@ declare module "earl" {
      * @param delta - The maximum difference in milliseconds between the value and now.
      */
     isCloseToNow(delta: number): never;
+
+    /**
+     * Check if the value is an array that is equal to the expected array, but not necessarily in the same order.
+     * 
+     * @param expected - The expected array. 
+     */
+    equalUnsorted<T>(expected: Array<T>): never;
   }
 }
 
 registerMatcher("isCloseToNow", isCloseToNow);
+registerMatcher("equalUnsorted", equalUnsorted);
 
 export function isCloseToNow(delta: number) {
   return (value: unknown): boolean => {
@@ -33,5 +41,15 @@ export function isCloseToNow(delta: number) {
     }
 
     return false;
+  };
+}
+
+export function equalUnsorted<T>(expected: Array<T>) {
+  return (value: unknown): boolean => {
+    console.log(JSON.stringify(Array.isArray(value)));
+    console.log(JSON.stringify((value as Array<any>).sort((a, b) => JSON.stringify(a).localeCompare(JSON.stringify(b)))));
+    console.log(JSON.stringify(expected.sort((a, b) => JSON.stringify(a).localeCompare(JSON.stringify(b)))));
+    
+    return Array.isArray(value) && isEqual(value.sort((a, b) => JSON.stringify(a).localeCompare(JSON.stringify(b))), expected.sort((a, b) => JSON.stringify(a).localeCompare(JSON.stringify(b))));
   };
 }
