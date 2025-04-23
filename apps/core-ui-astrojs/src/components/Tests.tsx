88c67857410b6d5d64704dc555420390af28ea46
Igor Lunin <lunigorn@gmail.com>
2025-04-23 22:34:51 +0200

    Add pagination

--- apps/core-ui-astrojs/src/components/Tests.tsx
+++ apps/core-ui-astrojs/src/components/Tests.tsx
@@ -1,6 +1,8 @@
 import { tsr } from "@/lib/react-query";
 import { contract } from "@total-report/core-contract/contract";
 import type { ClientInferResponseBody } from "@ts-rest/core";
+import { useEffect, useState } from "react";
+import { PaginationBlock } from "./pagination-block";
 import { RestAPIProvider } from "./RestAPIProvider";
 import { TestListItem, type Entity } from "./tests-list-item";
 import {
@@ -8,13 +10,28 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "./ui/resizable";
+import { ScrollArea } from "./ui/scroll-area";
 import { Separator } from "./ui/separator";
 
 const Internal = () => {
   const tsrQueryClient = tsr.useQueryClient();
 
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+
+  useEffect(() => {
+    if (page < 1) setPage(1);
+    if (pageSize < 1) setPageSize(10);
+  }, [page, pageSize]);
+
   const { data, isPending } = tsr.findTestEntities.useQuery({
-    queryKey: ["findTestEntities"],
+    queryKey: [`tests?page=${page}&pageSize=${pageSize}`],
+    queryData: {
+      query: {
+        limit: pageSize,
+        offset: (page - 1) * pageSize,
+      },
+    },
   });
 
   const statuses = tsr.findTestStatuses.useQuery({
@@ -34,45 +51,54 @@ const Internal = () => {
         autoSave="true"
         defaultValue={20}
         collapsible={false}
-        minSize={15}
+        minSize={25}
       >
         <div className="flex flex-col h-full">
           <div className="flex items-center px-4 py-2">
             <h1 className="text-xl font-bold">Tests</h1>
           </div>
           <Separator />
-          {(isPending || statuses.isPending || statusGroups.isPending) && (
-            <p className="p-4">Loading...</p>
-          )}
-          {!(isPending || statuses.isPending || statusGroups.isPending) &&
-            data?.body?.items.length === 0 && (
-              <div className="flex items-center justify-center flex-grow">
-                <div className="text-center">
-                  <p className="text-lg font-bold text-secondary-foreground">
-                    No tests found
-                  </p>
-                  <p className="text-sm text-muted-foreground mt-1">
-                    Try adjusting filters
-                  </p>
+          <ScrollArea className="flex-1 overflow-hidden">
+            {(isPending || statuses.isPending || statusGroups.isPending) && (
+              <p className="p-4">Loading...</p>
+            )}
+            {!(isPending || statuses.isPending || statusGroups.isPending) &&
+              data?.body?.items.length === 0 && (
+                <div className="flex items-center justify-center flex-grow">
+                  <div className="text-center">
+                    <p className="text-lg font-bold text-secondary-foreground">
+                      No tests found
+                    </p>
+                    <p className="text-sm text-muted-foreground mt-1">
+                      Try adjusting filters
+                    </p>
+                  </div>
                 </div>
-              </div>
-            )}
-          {!(isPending || statuses.isPending || statusGroups.isPending) &&
-            data?.body?.items != undefined &&
-            data?.body?.items.length > 0 && (
-              <div className="flex flex-col gap-2 p-2">
-                {data.body.items.map((test) => (
-                  <TestListItem
-                    entity={formatTest(
-                      test,
-                      statuses.data?.body.items!,
-                      statusGroups.data?.body.items!
-                    )}
-                    selected={false}
-                  />
-                ))}
-              </div>
-            )}
+              )}
+            {!(isPending || statuses.isPending || statusGroups.isPending) &&
+              data?.body?.items != undefined &&
+              data?.body?.items.length > 0 && (
+                <div className="flex flex-col gap-2 p-2">
+                  {data.body.items.map((test) => (
+                    <TestListItem
+                      entity={formatTest(
+                        test,
+                        statuses.data?.body.items!,
+                        statusGroups.data?.body.items!
+                      )}
+                      selected={false}
+                    />
+                  ))}
+                </div>
+              )}
+          </ScrollArea>
+          <PaginationBlock
+            page={page}
+            pageSize={pageSize}
+            totalItems={data?.body.pagination.total ?? 0}
+            setPage={setPage}
+            setPageSize={setPageSize}
+          ></PaginationBlock>
         </div>
       </ResizablePanel>
       <ResizableHandle withHandle />
