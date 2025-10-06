c35998bd3045636ae6496e3f03f0c537aac31edb
Igor Lunin <lunigorn@gmail.com>
2025-10-06 21:09:19 +0200

    Fix Skeleton colors

--- apps/core-ui/tailwind.config.ts
+++ apps/core-ui/tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
   theme: {
     extend: {
       colors: {
+        skeleton: "var(--skeleton)",
         background: "var(--background)",
         foreground: "var(--foreground)",
         card: {
