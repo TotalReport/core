f1f682b93eb4ea9df30243abafdeaf8aa25bc589
Igor Lunin <lunigorn@gmail.com>
2025-12-08 21:51:19 +0100

    Refactor tests

--- apps/core-ui/src/containers/tests/tests-container.tsx
+++
@@ -1,9 +0,0 @@
-import { TestsList } from '@/components/tests/tests-list.jsx';
-
-export const TestsContainer = () => {
-  return (
-    <div className="flex h-screen">
-      <TestsList />
-    </div>
-  );
-};
--- apps/core-ui/src/routes/tests/page.tsx
+++ apps/core-ui/src/routes/tests/page.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { RestAPIProvider } from '@/components/providers/rest-api-provider.jsx';
-import { TestsContainer } from '@/containers/tests/tests-container.jsx';
+import { TestsList } from '@/components/tests/tests-list.jsx';
 
 export const title = "Tests - Total Report";
 
-export default function Tests() {
+export default function TestsPage() {
   return (
     <RestAPIProvider>
-      <TestsContainer />
+      <TestsPageContent />
     </RestAPIProvider>
   );
 }
+
+const TestsPageContent = () => {
+  return (
+    <div className="flex h-screen">
+      <TestsList />
+    </div>
+  );
+};
